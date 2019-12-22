8f02597c2ee53847f939a16bd0d0f1c77cd2e936
markhulia <markhulia@gmail.com>
2019-12-22 22:45:34 +0100

    Consume messages from 1 bus line

--- bus_data.py
+++ bus_data.py
@@ -1,10 +1,50 @@
 from pykafka import KafkaClient
-
-client = KafkaClient(hosts="0.0.0.0:9092")
-topic = client.topics['busData']
-
-producer = topic.get_sync_producer()
-
-producer.produce('bus line 51'.encode('ascii'))
+import json
+from datetime import datetime
+import uuid
 
 
+def read_file(file):
+	with open(file, 'r') as f:
+		return json.load(f)
+
+
+def generate_uuid():
+	return uuid.uuid4()
+
+
+def generate_checkpoint(coordinates):
+	i = 0
+	 while i< len(coordinates):
+		data = {}
+		data['busline'] = '17'
+		data['key'] = f"{data['busline']}_{str(generate_uuid())}"
+		data['timestamp'] = str(datetime.utcnow())
+		data['longitude'] = coordinates[i][0]
+		data['latitude'] = coordinates[i][1]
+		yield json.dumps(data)
+
+		if i == len(coordinates)-1:
+			i = 0
+		else:
+			i+=1
+
+
+def main():
+	client = KafkaClient(hosts="0.0.0.0:9092")
+	topic = client.topics['busData']
+	producer = topic.get_sync_producer()
+	raw_data = []
+	raw_data.append(read_file('data/data_1.json'))
+	raw_data.append
+	coordinates = raw_data['features'][0]['geometry']['coordinates']
+
+	
+	for message in generate_checkpoint(coordinates):
+		
+		producer.produce(str(message).encode('ascii'))
+
+
+
+if __name__=="__main__":
+	main()
